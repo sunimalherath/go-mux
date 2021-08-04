361ea1d78d0119c1e663b0718be96d918d12d1ff
sunimalherath <sunimaldv@gmail.com>
2021-08-04 23:23:39 +1000

    removing incorrect files and folder

--- handlers/articles.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "net/http"
-
-func ArticlesHandler(w http.ResponseWriter, r *http.Request) {
-
-}
--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/sunimalherath/go-mux/handlers"
-)
-
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", handlers.HomeHandler)
-	r.HandleFunc("/products", handlers.ProductHandler)
-	r.HandleFunc("/articles", handlers.ArticlesHandler)
-	http.Handle("/", r)
+
 }
