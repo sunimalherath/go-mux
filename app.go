e92d8cd0069af0d32d457cfc857a6e1a2f37db3b
sunimalherath <sunimaldv@gmail.com>
2021-08-06 01:15:41 +1000

    implementing database connection

--- app.go
+++ app.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
@@ -14,7 +16,15 @@ type App struct {
 
 // Initialize method takes the data required to connect to the database
 func (a *App) Initialize(user, password, dbname string) {
+	connectionString := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", user, password, dbname)
 
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
 }
 
 // Run method starts the application
